f2fd79ad53307bca75a3ed2dfcca21e3fd649ca2
cclora <eduardamacedo0008@hotmail.com>
2020-03-05 14:59:46 -0300

    g

---
+++ src/components/Clock/WorldClock.js
@@ -0,0 +1,32 @@
+import React from 'react';
+class WorldClock extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          date: new Date()
+        };
+      }
+      componentDidMount() {
+        this.timerID = setInterval(
+          () => this.tick(),
+          1000
+        );
+      }
+    
+      tick() {
+        this.setState({
+          date: new Date()
+        });
+        this.state = {date: new Date};
+      }
+    
+      render() {
+        let {date} = this.state;
+        return (
+          <div>
+            <h2>wRODL clock is {date.toLocaleTimeString()}.</h2>
+          </div>
+        );
+      }
+  }
+  export default WorldClock;
